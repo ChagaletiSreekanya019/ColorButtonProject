d8c3e17530fd886ec77c8623185df6bb5f3d2040
ChagaletiSreekanya019 <103957807+ChagaletiSreekanya019@users.noreply.github.com>
2023-07-25 10:27:56 +0530

    Add files via upload

---
+++ color.js
@@ -0,0 +1,26 @@
+let box=document.getElementsByClassName("box");
+let red=document.getElementById("red");
+red.onclick=function(){
+    box[0].style.backgroundColor="red";
+}
+
+let yellow=document.getElementById("yellow");
+yellow.onclick=function(){
+    box[0].style.backgroundColor="yellow";
+}
+
+
+let green=document.getElementById("green");
+green.onclick=function(){
+    box[0].style.backgroundColor="green";
+}
+
+
+let blue=document.getElementById("blue");
+blue.onclick=function(){
+    box[0].style.backgroundColor="blue";
+}
+
+
+
+
